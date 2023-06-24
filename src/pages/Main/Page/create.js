dc34a1a6912a75fabc9670c3a91359a91e277008
Piyakarn Nimmakulvirut (Jimmy) <jirakarnjim1@gmail.com>
2023-06-24 16:29:12 +0700

    feat: update sort position

--- src/pages/Main/Page/create.js
+++ src/pages/Main/Page/create.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Row,
   Col,
@@ -14,6 +14,7 @@ import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const CreatePage = () => {
+  const [sortType, setSortType] = useState("upper");
   return (
     <>
       <div className="page-content">
@@ -39,20 +40,84 @@ const CreatePage = () => {
                         />
                       </div>
                     </Col>
-                    <Col md={12}>
-                      <Label
-                        htmlFor="example-text-input"
-                        className="col-md-2 col-form-Label"
-                      >
-                        Sort
-                      </Label>
-                      <div>
-                        <input
-                          className="form-control"
-                          type="number"
-                          placeholder="sort"
-                        />
+                    <Col md={3}>
+                      <Label className="col-md-2 col-form-Label">Sort</Label>
+                      <div className="d-flex gap-2">
+                        <div className="vstack gap-2">
+                          <div className="form-check">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="sortType"
+                              id="upper"
+                              value="upper"
+                              onChange={e => setSortType("upper")}
+                              checked={sortType === "upper"}
+                            />
+                            <label className="form-check-label" htmlFor="upper">
+                              Upper
+                            </label>
+                          </div>
+                        </div>
+                        <div className="vstack gap-2">
+                          <div className="form-check">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="sortType"
+                              id="middle"
+                              value="middle"
+                              onChange={e => setSortType("middle")}
+                              checked={sortType === "middle"}
+                            />
+                            <label
+                              className="form-check-label"
+                              htmlFor="middle"
+                            >
+                              Middle
+                            </label>
+                          </div>
+                        </div>
+                        <div className="vstack gap-2">
+                          <div className="form-check">
+                            <input
+                              className="form-check-input"
+                              type="radio"
+                              name="sortType"
+                              id="lower"
+                              value="lower"
+                              onChange={e => setSortType("lower")}
+                              checked={sortType === "lower"}
+                            />
+                            <label className="form-check-label" htmlFor="lower">
+                              Lower
+                            </label>
+                          </div>
+                        </div>
                       </div>
+                    </Col>
+                    <Col className="mt-2" md={12}>
+                      {sortType === "upper" || sortType === "lower" ? (
+                        <>
+                          <div>
+                            <input
+                              className="form-control"
+                              type="text"
+                              placeholder="Sort"
+                            />
+                          </div>
+                        </>
+                      ) : (
+                        <>
+                          <select className="form-control">
+                            <option>Select Position</option>
+                            <option>Between E-commerce and My Product</option>
+                            <option>
+                              Between My Product and Order History
+                            </option>
+                          </select>
+                        </>
+                      )}
                     </Col>
                     <Col md={12}>
                       <Label
